58972e7aea3bdb407623729b163507f3a4f74906
Alice (AntimonovM) <alice@example.com>
2020-03-23 19:21:37 +0400

    code: деление чисел

--- main.cpp
+++ main.cpp
@@ -11,6 +11,7 @@ int main()
     cin >> a >> b;
     cout << "A + B = " << a + b << '\n'
          << "A - B = " << a - b << '\n'
-         << "A * B = " << a * b << '\n';
+         << "A * B = " << a * b << '\n'
+         << "A / B = " << a / b << '\n';
     return 0;
 }
